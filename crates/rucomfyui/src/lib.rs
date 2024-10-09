ba3503e764fa6442eb3c32f361568827619b41c8
Philpax <me@philpax.me>
2024-10-09 14:33:12 +0200

    feat: Client::new_with_client

--- crates/rucomfyui/src/lib.rs
+++ crates/rucomfyui/src/lib.rs
@@ -32,11 +32,16 @@ pub struct Client {
     client: reqwest::Client,
 }
 impl Client {
-    /// Create a new client.
+    /// Create a new client with the default [`reqwest`] client.
     pub fn new(api_base: impl Into<String>) -> Self {
+        Self::new_with_client(api_base, reqwest::Client::default())
+    }
+    /// Create a new client with a custom [`reqwest`] client.
+    /// This is useful for setting custom timeouts, headers, etc.
+    pub fn new_with_client(api_base: impl Into<String>, client: reqwest::Client) -> Self {
         Self {
             api_base: api_base.into(),
-            client: reqwest::Client::new(),
+            client,
         }
     }
 }
